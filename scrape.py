aa78860e38dae3308b70dc522c7c185740abf4d1
sbhat <50996799+Suhailhassanbhat@users.noreply.github.com>
2021-07-26 15:09:20 -0400

    fadsdas

--- scrape.py
+++ scrape.py
@@ -7,7 +7,7 @@ import requests
 import numpy as np
 from bs4 import BeautifulSoup
 import re
-
+import ghostscript
 import camelot
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
